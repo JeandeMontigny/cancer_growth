c821fd2b6db594ed11af51005acfdc922a217fbf
Vasileios Vavourakis <vasvav@gmail.com>
2018-03-19 19:55:46 +0200

    Made some changes in the code to test the differentiation rules...

--- bio_module.h
+++ bio_module.h
@@ -14,20 +14,21 @@ namespace bdm {
       int growthSpeed;
       array<double, 3> cell_movements;
       double divideProba;
-      double currentOxygenLevel=cell->GetOxygenLevel();
+      double currentOxygenLevel = cell->GetOxygenLevel();
       // if normoxy: high division rate but low migration
       if (currentOxygenLevel > 0.7) {
         cell->SetHypoDiv(true);
-        growthSpeed=100;
-        cell_movements={gTRandom.Uniform(-1, 1), gTRandom.Uniform(-1, 1), gTRandom.Uniform(-1, 1)}; // create an array of 3 ramdom numbers between -1 and 1
-        divideProba=0.8;
+        growthSpeed = 100;
+        cell_movements = {gTRandom.Uniform(-1, 1), gTRandom.Uniform(-1, 1), gTRandom.Uniform(-1, 1)}; // create an array of 3 ramdom numbers between -1 and 1
+        // cell_movements = {gTRandom.Uniform(-4, 4), gTRandom.Uniform(-4, 4), gTRandom.Uniform(-4, 4)}; // create an array of 3 ramdom numbers between -4 and 4
+        divideProba = 0.9;
       }
       // if hypoxy: low division rate but high migration
       else if (currentOxygenLevel > 0.3) {
         growthSpeed = 40;
         //TODO: not a random migration. depending on oxygen gradient. Can get oxygen gradien from feb3
-        cell_movements={gTRandom.Uniform(-4, 4), gTRandom.Uniform(-4, 4), gTRandom.Uniform(-4, 4)}; // create an array of 3 ramdom numbers between -4 and 4
-        divideProba=0.4;
+        cell_movements = {gTRandom.Uniform(-4, 4), gTRandom.Uniform(-4, 4), gTRandom.Uniform(-4, 4)}; // create an array of 3 ramdom numbers between -4 and 4
+        divideProba = 0.4;
       }
       // necrose
       else {
@@ -38,15 +39,35 @@ namespace bdm {
         cell->SetHypoDiv(false);
       }
         
-      //cell grow until it reach a diam of 8
-      if (cell->GetDiameter() < 8) {
+      //cell grow until it reach a diam of ...
+      if (cell->GetDiameter() < 8.0) {
         cell->ChangeVolume(growthSpeed);
         cell->UpdateMassLocation(cell_movements);
         cell->SetPosition(cell->GetMassLocation());
         //Reset biological movement to 0.
         cell->SetTractorForce({0, 0, 0});
       }
+      else if (cell->GetDiameter() >= 8.0) {
+        cell->ChangeVolume(0.0);
+        cell->UpdateMassLocation(cell_movements);
+        cell->SetPosition(cell->GetMassLocation());
+        //Reset biological movement to 0.
+        cell->SetTractorForce({0, 0, 0});
+      }
+
+
+      if (cell->GetCanDivide() && cell->GetDiameter() > 7.0) {
+        double aNewRandomDouble=gTRandom.Uniform(0.0, 1.0);
+        if (aNewRandomDouble <= divideProba) { //0.55 //0.65
+          auto&& daughter = Divide(*cell);
+          daughter.SetCellColour(cell->GetCellColour()); // daughter takes the cell_colour_ value of her mother
+          daughter.SetCanDivide(true); // daughter will be able to divide
+          daughter.SetOxygenLevel(cell->GetOxygenLevel()); // daughter takes the oxygen_level_ value of her mother
+        }
+      }
+
       // when diam of 8, it has a chance of dividing (if it can divide)
+      /*
       else if (cell->GetCanDivide()==true && cell->GetHypoDiv()==true) {
         double aNewRandomDouble=gTRandom.Uniform(0, 1);
         if (aNewRandomDouble <= divideProba) { //0.55 //0.65
@@ -68,6 +89,7 @@ namespace bdm {
           }
         }
       }
+      */
       
     } // end of Run()
     
--- src/cancer_growth.h
+++ src/cancer_growth.h
@@ -14,7 +14,7 @@ namespace bdm {
 
   public:
     MyCellExt() {}
-  MyCellExt(const std::array<double, 3>& position) : Base(position) {} // our creator
+    MyCellExt(const std::array<double, 3>& position) : Base(position) {} // our creator
 
     // getter and setter for our new data member
     void SetCanDivide(bool d) { can_divide_[kIdx] = d; }
@@ -72,12 +72,14 @@ namespace bdm {
   } // end CellCreator
 
 
-  inline double get3DDistSq(array<double, 3> cell1, array<double, 3> cell2) {
+  inline
+  double get3DDistSq (array<double, 3> cell1, array<double, 3> cell2) {
     return pow((cell1[0]-cell2[0]),2)+pow((cell1[1]-cell2[1]),2)+pow((cell1[2]-cell2[2]), 2);
   }
 // 3.
   template <typename TResourceManager = ResourceManager<>>
-    inline int Simulate(int argc, const char** argv) {
+  inline
+  int Simulate (int argc, const char** argv) {
 
     size_t nb_of_cells=100; // 10000 // 1000000
     size_t nb_of_cancerous_cells=10; //10
@@ -85,42 +87,48 @@ namespace bdm {
     Param::live_visualization_ = false;
     Param::export_visualization_ = true;
     Param::visualization_export_interval_ = 8;
-    Param::visualize_sim_objects_["MyCell"] = std::set<std::string>{"diameter_", "cell_colour_"};
+    Param::visualize_sim_objects_["MyCell"] = std::set<std::string>{"diameter_", "cell_colour_", "oxygen_level_"};
    
     // Create an artificial bounds for the simulation space
     Param::bound_space_ = true;
     Param::min_bound_ = 0;
-    Param::max_bound_ = 1000;//1000
+    Param::max_bound_ = 100.0;//1000
     Param::run_mechanical_interactions_ = true;
     gTRandom.SetSeed(2448); // 5807
 
-// cell creation
     auto Construct_regular =  [](const std::array<double, 3>& position) {
       MyCell cell(position);
-      cell.SetDiameter(9);
+      cell.SetDiameter(5.0);
       cell.SetCellColour(0);
-//      cell.AddBiologyModule(GrowthModule());
-//      cell.SetCanDivide(false);
+      // cell.AddBiologyModule(GrowthModule());
+      cell.SetCanDivide(false);
+      cell.SetOxygenLevel(1.0);
+      // cell.SetHypoDiv(true);
       return cell;
     };
+
+    // cell creation
     CellCreator(Param::min_bound_, Param::max_bound_, nb_of_cells, Construct_regular);
     cout << "regular cells created" << endl;
 
     auto Construct_cancerous =  [](const std::array<double, 3>& position) {
       MyCell cell(position);
-      cell.SetDiameter(7);
-      cell.SetCellColour(2);
+      cell.SetDiameter(6.0);
+      cell.SetCellColour(1);
       cell.AddBiologyModule(GrowthModule());
       cell.SetCanDivide(true);
-      cell.SetOxygenLevel(1);
+      cell.SetOxygenLevel(1.0);
       cell.SetHypoDiv(true);
       return cell;
     };
-    CellCreator(450, 550, nb_of_cancerous_cells, Construct_cancerous); // 250, 350
+
+    // cell creation
+    CellCreator(45.0, 55.0, nb_of_cancerous_cells, Construct_cancerous); // 250, 350
     cout << "cancerous cells created" << endl;
 
     Scheduler<> scheduler;
-    int max_step=1000;
+    const int max_step = 1500;
+    // auto rm = TResourceManager::Get();
     
     for (int i=0; i<max_step; i++) {
       if (i%10==0) {
@@ -129,55 +137,51 @@ namespace bdm {
       scheduler.Simulate(1);
     }
 
-    auto rm = TResourceManager::Get();
-    auto my_cells=rm->template Get<MyCell>();
-    for (size_t i = 0; i < my_cells->size(); i++) {
-      auto&& cell = (*my_cells)[i];
-      if (cell.GetCellColour()==2) {
-        cell.SetOxygenLevel(0.5);
-      }
-    }
+    // auto my_cells=rm->template Get<MyCell>();
+    // for (size_t i = 0; i < my_cells->size(); i++) {
+    //   auto&& cell = (*my_cells)[i];
+    //   if (cell.GetCellColour()==2) {
+    //     cell.SetOxygenLevel(0.5);
+    //   }
+    // }
     
-    for (int i=0; i<max_step; i++) {
-      if (i%10==0) {
-        cout << "step " << i << " out of " << max_step << endl;
-      }      
-      scheduler.Simulate(1);
-    }
+    // for (int i=0; i<max_step; i++) {
+    //   if (i%10==0) {
+    //     cout << "step " << i << " out of " << max_step << endl;
+    //   }      
+    //   scheduler.Simulate(1);
+    // }
 
-    rm = TResourceManager::Get();
-    my_cells=rm->template Get<MyCell>();
-    for (size_t i = 0; i < my_cells->size(); i++) {
-      auto&& cell = (*my_cells)[i];
-      if (cell.GetCellColour()==2) {
-        cell.SetOxygenLevel(0.1);
-      }
-    }
+    // rm = TResourceManager::Get();
+    // my_cells=rm->template Get<MyCell>();
+    // for (size_t i = 0; i < my_cells->size(); i++) {
+    //   auto&& cell = (*my_cells)[i];
+    //   if (cell.GetCellColour()==2) {
+    //     cell.SetOxygenLevel(0.1);
+    //   }
+    // }
     
-    for (int i=0; i<max_step; i++) {
-      if (i%10==0) {
-        cout << "step " << i << " out of " << max_step << endl;
-      }      
-      scheduler.Simulate(1);
-    }
+    // for (int i=0; i<max_step; i++) {
+    //   if (i%10==0) {
+    //     cout << "step " << i << " out of " << max_step << endl;
+    //   }      
+    //   scheduler.Simulate(1);
+    // }
     
-    int cancerousCellNb=0;
-    rm = TResourceManager::Get();
-    my_cells=rm->template Get<MyCell>();
-    for (size_t i = 0; i < my_cells->size(); i++) {
-      auto&& cell = (*my_cells)[i];
-      if (cell.GetCellColour()==2) {
-        cancerousCellNb++;
-      }
-    }
-    cout << "nb of cancerous cells in the modelling: " << cancerousCellNb << endl;
-    cout << "done" << endl;
+    // int cancerousCellNb=0;
+    // rm = TResourceManager::Get();
+    // my_cells=rm->template Get<MyCell>();
+    // for (size_t i = 0; i < my_cells->size(); i++) {
+    //   auto&& cell = (*my_cells)[i];
+    //   if (cell.GetCellColour()==2) {
+    //     cancerousCellNb++;
+    //   }
+    // }
+    // cout << "nb of cancerous cells in the modelling: " << cancerousCellNb << endl;
+    // cout << "done" << endl;
+
     return 0;
     
-//    Exporter exporter;
-// exporter.CreatePVDFile("paraviewExport", 1, 1.0);
-//    exporter.ToFile(cells, "Positions.dat");
-//    exporter.ToVTUFile(cells, "viewExportVTU", 1);
   }
 
 } // namespace bdm
