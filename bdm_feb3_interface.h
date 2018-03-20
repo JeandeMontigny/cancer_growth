1cebb1803e10215411324443167b03f030e7094b
Vasileios Vavourakis <vasvav@gmail.com>
2018-03-20 20:56:13 +0200

    Some code restructuring in the interface header file.

--- bdm_feb3_interface.h
+++ bdm_feb3_interface.h
@@ -5,8 +5,6 @@
 #include <vector>
 #include <array>
 //=================================================================================================
-namespace bdm_feb3 {
-//=================================================================================================
 typedef double                 Real;
 typedef std::array<double, 3>  RealVector3d;
 //=================================================================================================
@@ -18,14 +16,20 @@ public:
     //
     inline
     void init (int n_cell_types, int n_biochem) {
-        this->cells_mass.assign(n_cell_types, 0.0);
-        this->cells_population.assign(n_cell_types, 0);
+        this->_n_cell_types = n_cell_types;
+        this->_n_biochem = n_biochem;
+        this->cells_mass.assign(this->_n_cell_types, 0.0);
+        this->cells_population.assign(this->_n_cell_types, 0);
         this->ecm_density = 0.0;
         this->ecm_density_gradient = RealVector3d({0.0, 0.0, 0.0});
-        this->biochemical_level.assign(n_biochem, 0.0);
-        this->biochemical_level_gradient.assign(n_biochem, RealVector3d({0.0, 0.0, 0.0}));
+        this->biochemical_level.assign(this->_n_biochem, 0.0);
+        this->biochemical_level_gradient.assign(this->_n_biochem, RealVector3d({0.0, 0.0, 0.0}));
         this->interstitial_fluid_velocity = RealVector3d({0.0, 0.0, 0.0});
     }
+    inline
+    unsigned int n_cell_types () const { return this->_n_cell_types; }
+    inline
+    unsigned int n_biochem () const { return this->_n_biochem; }
 public:
     std::vector<Real> cells_mass;
     std::vector<unsigned int> cells_population;
@@ -34,29 +38,26 @@ public:
     std::vector<Real>         biochemical_level;
     std::vector<RealVector3d> biochemical_level_gradient;
     RealVector3d interstitial_fluid_velocity;
+private:
+    unsigned int _n_cell_types, 
+                 _n_biochem;
 };
-//=================================================================================================
-} // end of namespace: bdm_feb3
-//=================================================================================================
-class BDM_CubicDomain {
+//-------------------------------------------------------------------------------------------------
+class BDM_Domain : public InterfaceData {
 public:
-    BDM_CubicDomain () : _is_init(false) {}
-    BDM_CubicDomain (int n_cell_types, int n_biochem) { this->init(n_cell_types, n_biochem); }
-    ~BDM_CubicDomain () {}
+    BDM_Domain () : _is_init(false) {}
+    BDM_Domain (int n_cell_types, int n_biochem) { BDM_Domain::init(n_cell_types, n_biochem); }
+    ~BDM_Domain () {}
     //
     inline
     void init (int n_cell_types, int n_biochem) {
-        this->data.init(n_cell_types, n_biochem);
-        // this->_cells = bdm::daosoa<bdm::Cell>(ncell);
         this->_is_init = true;
+        InterfaceData::init(n_cell_types, n_biochem);
     }
     inline
     bool is_init () const { return this->_is_init; }
-public:
-    bdm_feb3::InterfaceData data;
 private:
     bool _is_init;
-    // bdm::daosoa<bdm::Cell> _cells;
 };
 //=================================================================================================
 #endif
