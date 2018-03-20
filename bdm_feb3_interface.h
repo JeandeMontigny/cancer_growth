0607a0fbd73870a78931a1ba47d38e8f927a989a
Vasileios Vavourakis <vasvav@gmail.com>
2018-03-20 15:42:55 +0200

    Used the std::array instead to describe the 3D space vectos (or gradients of scalar quantities).

--- bdm_feb3_interface.h
+++ bdm_feb3_interface.h
@@ -3,26 +3,13 @@
 #define __BDM_FEB3_INTERFACE__
 //=================================================================================================
 #include <vector>
+#include <array>
 //=================================================================================================
 namespace bdm_feb3 {
 //=================================================================================================
-typedef double  Real;
+typedef double                 Real;
+typedef std::array<double, 3>  RealVector3d;
 //=================================================================================================
-class Vector3d {
-public:
-    Vector3d () { _comp[0]=_comp[1]=_comp[2]=0.0; }
-    Vector3d (const Real& x, const Real& y, const Real& z) { _comp[0]=x; _comp[1]=y; _comp[2]=z; }
-    Vector3d (const Vector3d& v) { _comp[0]=v(0); _comp[1]=v(1); _comp[2]=v(2); }
-    ~Vector3d () {}
-    //
-    inline
-    Real& operator() (unsigned int i) { return _comp[i]; }
-    inline
-    const Real& operator() (unsigned int i) const { return _comp[i]; }
-private:
-    Real _comp[3];
-};
-//-------------------------------------------------------------------------------------------------
 class InterfaceData {
 public:
     InterfaceData () {}
@@ -34,19 +21,19 @@ public:
         this->cells_mass.assign(n_cell_types, 0.0);
         this->cells_population.assign(n_cell_types, 0);
         this->ecm_density = 0.0;
-        this->ecm_density_gradient = Vector3d();
+        this->ecm_density_gradient = RealVector3d({0.0, 0.0, 0.0});
         this->biochemical_level.assign(n_biochem, 0.0);
-        this->biochemical_level_gradient.assign(n_biochem, Vector3d());
-        this->interstitial_fluid_velocity = Vector3d();
+        this->biochemical_level_gradient.assign(n_biochem, RealVector3d({0.0, 0.0, 0.0}));
+        this->interstitial_fluid_velocity = RealVector3d({0.0, 0.0, 0.0});
     }
 public:
     std::vector<Real> cells_mass;
     std::vector<unsigned int> cells_population;
-    Real     ecm_density;
-    Vector3d ecm_density_gradient;
-    std::vector<Real>     biochemical_level;
-    std::vector<Vector3d> biochemical_level_gradient;
-    Vector3d interstitial_fluid_velocity;
+    Real         ecm_density;
+    RealVector3d ecm_density_gradient;
+    std::vector<Real>         biochemical_level;
+    std::vector<RealVector3d> biochemical_level_gradient;
+    RealVector3d interstitial_fluid_velocity;
 };
 //=================================================================================================
 } // end of namespace: bdm_feb3
